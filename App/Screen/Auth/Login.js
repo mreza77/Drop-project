f77e095ac2c381a4d59d562737ff973a03764cf8
mohammad reza <mojaver77@gmail.com>
2020-12-15 00:54:32 +0330

    add api to compo

--- App/Screen/Auth/Login.js
+++ App/Screen/Auth/Login.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
-import { View, Text, Image, TouchableOpacity, ScrollView } from 'react-native';
+import { View, Text, Image, TouchableOpacity, ScrollView, StatusBar } from 'react-native';
 import { Styles } from '../../Style/GlobalStyle';
 import { Formik } from 'formik';
 import * as Yup from "yup";
 import { FormFieldTextInput, FormFieldTextBox, FormButton } from '../../Components/Formik';
+import Global from '../../Global/Global';
+import Axios from 'axios';
+import Loading from '../../Components/Loading/Loading';
+import Alert from 'react-native-android-alert-mjr';
 
 const validationSchema = Yup.object().shape({
-  Phone: Yup.number().required(),
+  Email: Yup.string().required().email(),
   Password: Yup.string().required().min(4),
 })
 
@@ -14,33 +18,92 @@ class Login extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      data: {},
+      visibleLoading: false,
+      visiblealert: false,
+      Alert: ""
     };
   }
 
+  SendData = () => {
+    this.setState({ visibleLoading: true }, () => {
+      Axios.post(`${Global.baseUrl}api/login`, {
+        email: this.state.data.Email,
+        password: this.state.data.Password
+      })
+        .then(response => {
+          this.setState({ visibleLoading: false }, () => {
+            console.log(response.data)
+            if (response.data) {
+              if (response.data.token) {
+                console.log(response.data.token)
+                this.props.navigation.navigate("Home")
+              } else {
+                this.setState({
+                  visiblealert: true,
+                  Alert: response.data.error
+                })
+              }
+            } else {
+              this.setState({
+                visiblealert: true,
+                Alert: "Problem in connection process, try again"
+              })
+            }
+          })
+        })
+        .catch(error => {
+          this.setState({ visibleLoading: false }, () => {
+            setTimeout(() => {
+              this.setState({
+                visiblealert: true,
+                Alert: "Internet connection error ..."
+              })
+            }, 1000)
+          })
+        })
+    })
+  }
+
+
   render() {
+    console.log(Global.baseUrl)
     return (
-      <ScrollView showsVerticalScrollIndicator={false}>
-        <View style={Styles.body}>
-          <View style={Styles.ContainerLogo}>
-            <Image
-              source={require("../../Assets/Pngs/logo.png")}
-              style={Styles.LogoLogin}
-            ></Image>
-          </View>
-          <View style={Styles.Loginbody}>
+
+
+      <View style={Styles.body}>
+        <StatusBar backgroundColor={"#fff"} barStyle={"dark-content"} />
+        <Loading isVisible={this.state.visibleLoading} />
+        <Alert
+          isVisible={this.state.visiblealert}
+          text={this.state.Alert}
+          closeText={"close"}
+          onClose={() => {
+            this.setState({ visiblealert: false })
+          }}
+        />
+        <View style={Styles.ContainerLogo}>
+          <Image
+            source={require("../../Assets/Pngs/logo.png")}
+            style={Styles.LogoLogin}
+            resizeMode={"center"}
+          ></Image>
+        </View>
+        <View style={Styles.Loginbody}>
+          <ScrollView showsVerticalScrollIndicator={false}>
             <Text style={Styles.TextLogin}>Welcome</Text>
             <Formik
-              initialValues={{ Phone: "", Password: "" }}
-              onSubmit={(values) => { console.log(values) }}
+              initialValues={{ Email: "", Password: "" }}
+              onSubmit={(values) => { this.setState({ data: values }, () => { this.SendData() }) }}
               validationSchema={validationSchema}
             >
               {() => (
                 <>
                   <FormFieldTextInput
-                    placeholder={"Phone"}
+                    placeholder={"Email"}
                     placeholderTextColor={"#000"}
-                    keyboardType={"phone-pad"}
-                    name={"Phone"}
+                    name={"Email"}
+                    icon={"email"}
                   />
                   <FormFieldTextBox
                     placeholder={"Password"}
@@ -70,9 +133,10 @@ class Login extends Component {
                       </Text>
               </TouchableOpacity>
             </View>
-          </View>
+          </ScrollView>
         </View>
-      </ScrollView>
+      </View>
+
     );
   }
 }
